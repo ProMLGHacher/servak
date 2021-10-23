8548c0350f137deb308e5e7acb78a159c8c5a127
ProMLGHacher <itismymessagebox@mail.ru>
2021-10-23 19:42:50 +0300

    geocode

--- src/main/java/com/example/server/controller/UserController.java
+++ src/main/java/com/example/server/controller/UserController.java
@@ -27,11 +27,55 @@ public class UserController {
         }
     }
 
-    @GetMapping("/route")
-    public String Ok(String str0, String str1) {
+    public String Geocod(String str) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.mapbox.com/directions/v5/mapbox/cycling/"+ str0 + ";" + str1 +"?geometries=geojson&access_token=sk.eyJ1IjoicXdlcnR5anVzdHRlc3QiLCJhIjoiY2t2M250ZjZ2NDlucDJvczd5Z21wMXA0biJ9.OoflJ5Q7AcDGws02P3dDGw"))
+                .uri(URI.create("https://nominatim.openstreetmap.org/search.php?q=" + str + "&format=jsonv2"))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String bicycle = response.body();
+        int a = bicycle.indexOf("osm_type\":\"") + 11;
+        int b = bicycle.indexOf("osm_id\":") + 8;
+        int c = bicycle.indexOf(",\"boundingbox");
+        String cha = String.valueOf(bicycle.charAt(a));
+        return Ok3(cha.toUpperCase(),bicycle.substring(b,c));
+    }
+
+    public String Ok3(String str0,String str1) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://nominatim.openstreetmap.org/details.php?osmtype=" + str0 + "&osmid=" + str1 + "&addressdetails=1&hierarchy=0&group_hierarchy=1&format=json"))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int a = response.body().indexOf("coordinates\":[") + 14;
+        int b = response.body().indexOf("]},\"geometry\":{\"type\":\"Point\",\"coordinates\":");
+        String[] str = response.body().substring(a,b).split(",");
+        String bicycle = str[1] + "," + str[0];
+        return bicycle;
+    }
+
+    @GetMapping("/route")
+    public String Ok(String str0, String str1) {
+
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.mapbox.com/directions/v5/mapbox/cycling/"+ Geocod(str0) + ";" + Geocod(str1) +"?geometries=geojson&access_token=sk.eyJ1IjoicXdlcnR5anVzdHRlc3QiLCJhIjoiY2t2M250ZjZ2NDlucDJvczd5Z21wMXA0biJ9.OoflJ5Q7AcDGws02P3dDGw"))
                 .build();
         HttpResponse<String> response = null;
         try {
